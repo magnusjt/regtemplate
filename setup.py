1dfee67043c29b74c54ea93686b920cc5d857629
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-01-15 15:34:07 +0100

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "regtemplate",
+    version = "1",
+    description = "Parse templates into regexes, and use those to match text into name-value pairs",
+    license = 'MIT',
+    author = 'Magnus Jeffs Tovslid',
+    packages = ['regtemplate'],
+)
